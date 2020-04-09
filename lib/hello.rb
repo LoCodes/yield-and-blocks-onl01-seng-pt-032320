a62aa78c490683fd5af35556e661172d5243ec4f
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-09 01:28:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,10 +3,10 @@ def hello_t(array)
 
   while i < array.length    #hello_t
     yield array[i]             #calls the block once for each element in the passed-in array
-    i = i + 1
-  end
-
-  array                        #returns the original array
+    i = i + 1                #Tim
+  end                        #Tom
+                             #Jim
+  array         #returns the original array
 end
 
 # call your method here!
@@ -17,6 +17,8 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
 end
 
+
+#fails gracefully when a block is not passed in
 
 def hello_t(array)
   if block_given?
